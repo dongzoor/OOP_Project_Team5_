a85c62bc05d89f835eba4ef30ef00956a25ba0d7
dongzoor <dongjoo92@gmail.com>
2016-11-18 15:09:55 +0900

    user and member

--- Team5_Oop_Project/src/ajou/ac/kr/team5/User.java
+++ Team5_Oop_Project/src/ajou/ac/kr/team5/User.java
@@ -1,8 +1,92 @@
 package ajou.ac.kr.team5;
 
-public class User {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-	public static void main(String[] args) {
+public class User{
+	
+	
+	public void Save(ArrayList<Member> memberlist) throws Exception{
+		
+		FileOutputStream fos = new FileOutputStream("user.txt");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        
+        oos.writeObject(memberlist);
+        
+        fos.close();
+        oos.close();
+        
+	}
+	
+	public ArrayList<Member> Load (ArrayList<Member> memberlist) throws IOException, ClassNotFoundException{
+		FileInputStream fis = new FileInputStream("user.txt");
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        
+        memberlist = (ArrayList<Member>)ois.readObject();
+        
+        fis.close();
+        ois.close();
+       
+        return memberlist;
+	
+	}
+	
+	public static void show(ArrayList<Member> memberlist) {
+       
+        System.out.println(" 전체 학생 출력");
+        System.out.println("==========================================================");
+        for (Member student : memberlist) {
+            System.out.format("id : %s 이름 : %s  : %d \n",student.getId(), student.getName(), student.getAge());
+        }
+        System.out.println("==========================================================");
+    }
+	public static void main(String[] args) throws Exception {
+		
+		ArrayList<Member> memberlist = new ArrayList();
+		ArrayList<Member> checklist = new ArrayList();
+		Scanner sc = new Scanner(System.in);
+		
+		User user = new User();
+		String id;
+		String password;
+		
+		System.out.println("Hello, Student!");
+		System.out.print("ID: ");
+		id = sc.nextLine();
+		System.out.print("PASSWORD: ");
+		password = sc.nextLine();
+		
+		/*
+		user.Join(memberlist);
+		user.Save(memberlist);*/
+	
+		checklist = user.Load(memberlist);
+		
+		user.show(memberlist);
+		
+		if(checklist.contains(id) == false){ //가입이 안돼있으면,
+			
+			System.out.print("NAME: ");
+			String name = sc.next();
+			
+			System.out.print("AGE: ");
+			int age = sc.nextInt();
+			
+			Member member = new Member(id,password,name,age);
+			
+			memberlist.add(member);
+
+			user.Save(memberlist);
+		}
+			
+		user.show(memberlist);
 		
 	}
+
 }
